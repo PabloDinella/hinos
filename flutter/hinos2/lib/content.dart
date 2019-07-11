ffd59fdd665612e727422380c4bf2a73a050d111
Pablo Rocha Dinella <pablo.dinella@gmail.com>
2019-07-10 23:24:22 -0300

    (re)starting development from a ready app done in a tutorial (https://github.com/shubie/Beautiful-List-UI-and-detail-page), this is already boosting my learning!

---
+++ flutter/hinos2/lib/content.dart
@@ -0,0 +1,62 @@
+import 'package:beautiful_list/model/lesson.dart';
+
+List getLessons() {
+  return [
+    Lesson(
+        number: '1',
+        title: "ABA-PAI, A TI CHEGAMOS",
+        level: "Beginner",
+        indicatorValue: 0.33,
+        price: 20,
+        content:
+            "strofe 1\n\nasdf 2"),
+    Lesson(
+        number: '2',
+        title: "Observation at Junctions",
+        level: "Beginner",
+        indicatorValue: 0.33,
+        price: 50,
+        content:
+            "Start by taking a couple of minutes to read the info in this section. Launch your app and click on the Settings menu.  While on the settings page, click the Save button.  You should see a circular progress indicator display in the middle of the page and the user interface elements cannot be clicked due to the modal barrier that is constructed."),
+    Lesson(
+        number: '3',
+        title: "Reverse parallel Parking",
+        level: "Intermidiate",
+        indicatorValue: 0.66,
+        price: 30,
+        content:
+            "Start by taking a couple of minutes to read the info in this section. Launch your app and click on the Settings menu.  While on the settings page, click the Save button.  You should see a circular progress indicator display in the middle of the page and the user interface elements cannot be clicked due to the modal barrier that is constructed."),
+    Lesson(
+        number: '4',
+        title: "Reversing around the corner",
+        level: "Intermidiate",
+        indicatorValue: 0.66,
+        price: 30,
+        content:
+            "Start by taking a couple of minutes to read the info in this section. Launch your app and click on the Settings menu.  While on the settings page, click the Save button.  You should see a circular progress indicator display in the middle of the page and the user interface elements cannot be clicked due to the modal barrier that is constructed."),
+    Lesson(
+        number: '5',
+        title: "Incorrect Use of Signal",
+        level: "Advanced",
+        indicatorValue: 1.0,
+        price: 50,
+        content:
+            "Start by taking a couple of minutes to read the info in this section. Launch your app and click on the Settings menu.  While on the settings page, click the Save button.  You should see a circular progress indicator display in the middle of the page and the user interface elements cannot be clicked due to the modal barrier that is constructed."),
+    Lesson(
+        number: '6',
+        title: "Engine Challenges",
+        level: "Advanced",
+        indicatorValue: 1.0,
+        price: 50,
+        content:
+            "Start by taking a couple of minutes to read the info in this section. Launch your app and click on the Settings menu.  While on the settings page, click the Save button.  You should see a circular progress indicator display in the middle of the page and the user interface elements cannot be clicked due to the modal barrier that is constructed."),
+    Lesson(
+        number: '7',
+        title: "Self Driving Car",
+        level: "Advanced",
+        indicatorValue: 1.0,
+        price: 50,
+        content:
+            "Start by taking a couple of minutes to read the info in this section. Launch your app and click on the Settings menu.  While on the settings page, click the Save button.  You should see a circular progress indicator display in the middle of the page and the user interface elements cannot be clicked due to the modal barrier that is constructed.  ")
+  ];
+}
